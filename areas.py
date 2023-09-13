1cb460757392fe1f928e77878f8c77eb4b0d1bfc
Felix Moro <FelixMoro.MrDogit@gmail.com>
2023-09-13 18:32:27 +0300

    added int | float + ignore pycache

--- areas.py
+++ areas.py
@@ -1,4 +1,7 @@
-"""This module calculates the areas of various shapes"""
+"""
+This module calculates the areas of various shapes.
+Requiers python 3.10+
+"""
 
 """
 Задание:
@@ -14,10 +17,10 @@
 """
 from math import pi, sqrt
 
-def circle(radius:int)->int:
+def circle(radius: int | float)->int:
     return radius ** 2 * pi
-
-def triangle(side_1:int, side_2:int, side_3:int) -> int:
+# (data: Union[int, float])
+def triangle(side_1: int | float, side_2: int | float, side_3: int | float) -> float:
     perimeter = side_1 + side_2 + side_3
     semiperimeter = perimeter / 2
     area = side_1 + side_2 + side_3
