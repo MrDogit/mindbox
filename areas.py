b6c80640ff6034c37b23224b3357c9c3bfecb372
Felix Moro <FelixMoro.MrDogit@gmail.com>
2023-09-15 11:26:12 +0300

    rewritten to classes

--- areas.py
+++ areas.py
@@ -1,31 +1,30 @@
-"""
-This module calculates the areas of various shapes.
-Requiers python 3.10+
-"""
-
-"""
-Задание:
-
-Напишите на C# или Python библиотеку для поставки внешним клиентам, 
-которая умеет вычислять площадь круга по радиусу и треугольника по трем сторонам. 
-Дополнительно к работоспособности оценим:
-
-- Юнит-тесты
-- Легкость добавления других фигур
-- Вычисление площади фигуры без знания типа фигуры в compile-time
-- Проверку на то, является ли треугольник прямоугольным
-"""
+"""This module calculates the areas of various shapes.
+Requiers python 3.10 or higher."""
 from math import pi, sqrt
 
-def circle(radius: int | float) -> float:
-    return radius ** 2 * pi
+class Circle():
+    def __init__(self, radius: int | float):
+        self.radius = radius
+        
+        self.area = self.radius ** 2 * pi
 
-def triangle(side_1: int | float, side_2: int | float, side_3: int | float) -> float:    
-    perimeter = side_1 + side_2 + side_3
-    semiperimeter = perimeter / 2
-    
-    area = sqrt(semiperimeter
-                * (semiperimeter - side_1)
-                * (semiperimeter - side_2)
-                * (semiperimeter - side_3))
-    return area
+class Triangle():
+    def __init__(self, side_1: int | float, side_2: int | float, side_3: int | float):
+        self.side_1 = side_1
+        self.side_2 = side_2
+        self.side_3 = side_3
+
+        self.perimeter = self.side_1 + self.side_2 + self.side_3
+        semiperimeter = self.perimeter / 2
+        self.area = sqrt(semiperimeter
+                    * (semiperimeter - self.side_1)
+                    * (semiperimeter - self.side_2)
+                    * (semiperimeter - self.side_3))
+        
+        if (side_1**2 + side_2**2 == side_3**2)\
+        or (side_1**2 + side_3**2 == side_2**2)\
+        or (side_3**2 + side_2**2 == side_1**2):
+            
+            self.is_right = True 
+        else:
+            self.is_right = False
--- test_areas.py
+++ test_areas.py
@@ -5,11 +5,14 @@ import areas
 class TestAreas(unittest.TestCase):
     
     def test_circle(self):
-        self.assertAlmostEqual(areas.circle(5.6418958354776), 100)
-        self.assertAlmostEqual(areas.circle(10), 314.15926535897932384626433)
+        self.assertAlmostEqual(areas.Circle(5.6418958354776).area, 100)
+        self.assertAlmostEqual(areas.Circle(10).area, 314.15926535897932384626433)
     
     def test_triangle(self):
-        self.assertEqual(areas.triangle(3, 4, 5), 6)
+        self.assertEqual(areas.Triangle(3, 4, 5).area, 6)
+        self.assertEqual(areas.Triangle(3, 4, 5).is_right, True)
+        self.assertEqual(areas.Triangle(3, 4, 6).is_right, False)
+
 
 if __name__ == '__main__':
     unittest.main()
